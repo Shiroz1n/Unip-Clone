e3fcdb6ad82ef39de852162d8257ae1b2db451a9
Alan Inhuma <aspas_inhuma321@outlook.com>
2026-07-15 22:15:07 -0300

    Fech para json

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../AuthProvider/AuthProvider";
-import usuarios from "./usuarios.json"
 
 const Login = () => {
     const { login } = useAuth();
     const [idInput, setIdInput] = useState("");
     const [senhaInput, setSenhaInput] = useState("");
     const [erro, setErro] = useState("");
+    const [usuarios, setUsuarios] = useState([]);
 
     const navigate = useNavigate();
 
+    
+    useEffect(() => {
+        fetch('./usuarios.json')
+            .then((res) => res.json())
+            .then((data) => setUsuarios(data))
+            .catch((err) => console.error("Erro ao carregar usuários:", err));
+    }, []);
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        
         const usuarioEncontrado = usuarios.find(
             (user) => user.id.toLowerCase() === idInput.toLowerCase() && user.senha === senhaInput
         );
+
         if (usuarioEncontrado) {
             login(usuarioEncontrado);
             navigate('/MainMenu');
@@ -25,14 +35,14 @@ const Login = () => {
     };
 
     const isFormValid = idInput.trim() !== "" && senhaInput.trim() !== "";
+    
     return (
-        // Container que ocupa a tela toda com o fundo azul (substitua a cor pelo seu fundo/imagem)
         <div className="min-h-screen flex flex-col items-center pt-20 bg-[url('/Imagens/header_01.png')] bg-cover bg-center bg-no-repeat">
             
             {/* Logo */}
             <img src="/Imagens/logo-unip.svg" alt="Logo UNIP" className="mb-4 w-48" />
 
-            {/* Caixa de login: Sólida, sem arredondamento, sem blur */}
+            {/* Caixa de login */}
             <div className=" p-8 w-full max-w-sm shadow-lg">
                 
                 <h2 className="text-xl font-bold mb-6 text-center text-white">Área do(a) Aluno(a)</h2>
@@ -56,7 +66,7 @@ const Login = () => {
 
                     <button 
                         type="submit"
-                        disabled={!isFormValid} // Botão desativado se o form estiver vazio
+                        disabled={!isFormValid}
                         className={`w-full p-3 transition-colors font-bold text-white rounded-lg border-gray-100 ${
                             isFormValid ? "bg-[#28a745] hover:bg-green-700" : "bg-[#28a745] opacity-50 cursor-not-allowed"
                         }`}
@@ -71,7 +81,6 @@ const Login = () => {
                     </p>
                 )}
 
-                {/* Links estilizados conforme a imagem */}
                 <div className="mt-6 flex justify-center gap-4 text-sm">
                     <a href="#" className="border border-blue-500 px-3 py-1 text-blue-500! hover:bg-blue-300 hover:text-white transition-colors rounded-sm">Esqueci a senha</a>
                     <a href="#" className="border border-blue-500 px-3 py-1 text-blue-300! hover:bg-blue-400 hover:text-white-500 transition-colors rounded-sm">Como acessar</a>
