c493bfa7ce42062f9fcfc45741b850ac5101c908
Alan Inhuma <aspas_inhuma321@outlook.com>
2026-07-15 22:07:02 -0300

    Ajustado caminho json

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../AuthProvider/AuthProvider";
-import usuarios from "/home/alan/repos/Unip-Clone/usuarios.json"
+import usuarios from "./usuarios.json"
 
 const Login = () => {
     const { login } = useAuth();
